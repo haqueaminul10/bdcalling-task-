5a213b1d1013e2eac0059bbc768357d7f9f7db6d
haqueaminul10 <haqueaminul10100@gmail.com>
2025-01-11 05:57:02 +0600

    special section doing

--- src/components/hero/AboutFreshHarvest.jsx
+++ src/components/hero/AboutFreshHarvest.jsx
@@ -3,10 +3,11 @@ import { Button } from '../ui/button';
 import leaf from '../assets/leaf.png';
 import Image from 'next/image';
 import logo from '../assets/Logo.png';
+import janina from '../assets/16.png';
 const AboutFreshHarvest = () => {
   return (
     <>
-      <div className='flex justify-between gap-4 mx-32 my-20 items-center relative'>
+      <div className='flex justify-between gap-4 mx-32 my-20 items-center relative '>
         <section
           className='w-1/2'
           style={{
@@ -19,13 +20,22 @@ const AboutFreshHarvest = () => {
           <div className='absolute left-[40%] top-[20%]'>
             <Image src={leaf} alt='' height={51} width={67} />
           </div>
-          <div className='absolute top-[70%] left-[25%] flex items-center bg-gray-100 p-1'>
+          <div className='absolute top-[65%] left-[25%] flex items-center bg-gray-100 p-1'>
             <div>
               {' '}
               <Image src={logo} alt='' height={14} width={24} />
             </div>
 
             <h3 className='text-[26px] font-semibold'> Fresh Harvests</h3>
+          </div>
+
+          <div className='absolute top-[75%] left-[30%] bg-gray-200 w-[140px] h-[230px]'>
+            <Image src={janina} alt='' height={110} width={140} />
+            <h3 className='text-center'>Mushroom</h3>
+            <p className='text-center'>$3.2/kg</p>
+            <div className='flex justify-center'>
+              <Button variant='outline'>Add to Cart</Button>
+            </div>
           </div>
         </section>
         <section className='w-1/2'>
--- src/components/hero/SpecialOffer.jsx
+++ src/components/hero/SpecialOffer.jsx
@@ -1,16 +1,38 @@
 import bg from '@/components/assets/Bg.png';
+import { Button } from '../ui/button';
 const SpecialOffer = () => {
   return (
-    <>
-      <div
-        style={{
-          backgroundImage: `url(${bg.src})`,
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-          height: '100vh',
-        }}
-      ></div>
-    </>
+    <div
+      style={{
+        backgroundImage: `url(${bg.src})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        height: '100vh',
+      }}
+    >
+      <div className='px-32 py-12'>
+        <Button
+          variant='outlline'
+          className='text-green-500 border border-gray-300 p-4 mb-4'
+        >
+          Special offer
+        </Button>
+        <h1 className='text-[80px]'>Seasonal Fruit Bundle</h1>
+        <h3>
+          Discount up to <span>80% OFF</span>
+        </h3>
+        <div>
+          <div>
+            <h2></h2>
+            <p></p>
+          </div>
+        </div>
+        <Button variant='outline' className='bg-[#176d38]'>
+          {' '}
+          CODE: <span>FRUIT28</span>
+        </Button>
+      </div>
+    </div>
   );
 };
 
